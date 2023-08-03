0fbba7670052fe137f67e737f69d5863299c4e8d
Riccardo <r.burger92@icloud.com>
2023-08-03 16:41:17 +0200

    BlackJack Aufgabe gelöst, System.out.printf verwendet, Math.max probiert.

---
+++ src/Kapitel2_ImperativeSprachkonzepte/BottlesOfRum_235.java
@@ -0,0 +1,31 @@
+package Kapitel2_ImperativeSprachkonzepte;
+
+/**
+ * 2.3.5 Eine Flasche Rum, 10 Flaschen Rum ★
+ * Bonny Brain ist eine Jägerin der falschen Sprache, und sie achtet immer darauf, dass
+ * Beschriftungen grammatikalisch korrekt sind. Die Regeln für den Plural sind in vielen
+ * Sprachen besonders, denn es heißt zum Beispiel »1 Flasche« oder »99 Flaschen«,
+ * aber auch »0 Flaschen«. Oftmals finden sich in Oberflächen Vereinfachungen wie
+ * »1 Flasche(n)«.
+ * Aufgabe:
+ *  Lege eine Variable noOfBottles an, und belege sie mit einem Wert größer gleich 0.
+ *  Programmiere eine grammatikalisch korrekte Ausgabe, abhängig davon, ob es 0, 1
+ * oder viele Flaschen gibt.
+ * Beispiel:
+ *  "0 bottles of rum"
+ *  "1 bottle of rum"
+ *  "99 bottles of rum"
+ */
+public class BottlesOfRum_235 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Wieviele Flaschen sind noch im Lager?");
+        int nrOfBottles = new java.util.Scanner(System.in).nextInt();
+
+       String bottles = nrOfBottles > 1 || nrOfBottles == 0 ? "Flaschen" : "Flasche";
+
+        System.out.printf("%d %s im Lager! %n", nrOfBottles, bottles);
+
+    }
+}
---
+++ src/Kapitel2_ImperativeSprachkonzepte/EinundZwanzig_236.java
@@ -0,0 +1,44 @@
+package Kapitel2_ImperativeSprachkonzepte;
+
+/**
+ * 2.3.6 Einundzwanzig ★
+ * Im Casino von Captain CiaoCiao spielt der Croupier gegen einen Spieler das Spiel
+ * »Einundzwanzig«. Ziel ist es, mit zwei oder mehr Karten näher an 21 Punkte heranzukommen
+ * als der Croupier.
+ * Aufgabe:
+ *  Lies zwei positive Ganzzahlen dealer und player für die erreichte Punktzahl von
+ * Croupier und Spieler ein.
+ *  Gib genau den Wert aus, der näher an 21 liegt. Geht ein Wert über 21, hat die andere
+ * Seite gewonnen. Gehen beide Werte über 21, soll 0 ausgegeben werden.
+ * Beispiel:
+ *  Eingaben 21 und 18 → Ausgabe 21
+ *  Eingaben 18 und 21 → Ausgabe 21
+ *  Eingaben 21 und 21 → Ausgabe 21
+ *  Eingaben 22 und 23 → Ausgabe 0
+ *  Eingaben 1 und 10 → führen zum Programmende
+ *  Eingaben 1 und 22 → führen zum Programmende
+ */
+public class EinundZwanzig_236 {
+    public static void main(String[] args) {
+
+        boolean spielAktiv = true;
+
+        while (spielAktiv) {
+            System.out.printf("Bitte die Summe beider Karten des Dealers eingeben: ");
+            int zahlenDealer = new java.util.Scanner(System.in).nextInt();
+            System.out.printf("Bitte die Summe beider Karten des Spielers eingeben: ");
+            int zahlenSpieler = new java.util.Scanner(System.in).nextInt();
+            int maxScore = 21;
+
+            if (zahlenSpieler > maxScore && zahlenDealer > maxScore) {
+                System.out.printf("Beide drüber. %n");
+            } else if ( zahlenSpieler > maxScore) {
+                System.out.println(zahlenDealer + " Dealer");
+            } else if (zahlenDealer > maxScore) {
+                System.out.println(zahlenSpieler + " Spieler");
+            } else
+                System.out.println(Math.max(zahlenDealer, zahlenSpieler));
+
+        }
+    }
+}
